de7225f625067ab3992de836c8be495b0347f8eb
Evan Wallis <evanwallis123@gmail.com>
2026-01-29 21:56:13 -0500

    Externalize libsql packages for Next build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: [
+    "@prisma/adapter-libsql",
+    "@libsql/client",
+    "@libsql/hrana-client",
+    "@libsql/isomorphic-fetch",
+  ],
 };
 
 module.exports = nextConfig;
